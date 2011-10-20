f621547ad8bc649b45ac355f73cc65fc7df9b178
Max Burstyn <max@burstyn.ca>
2011-10-20 20:20:40 +0000

    Set timezone to eastern

--- script_init.py
+++ script_init.py
@@ -1,3 +1,8 @@
 # Common initialization for scripts
+import os
 import sys
+
+# Allow omission of the initial 'cortex.' for all import statements
 sys.path.append(sys.path[0] + "/cortex")
+# Set timezone to eastern
+os.environ['TZ'] = 'US/Eastern'
